e18bc1c753cbff69087abf3c40a2722d6bdbd25f
theinfosecguy <keshavaarav22@gmail.com>
2022-03-13 21:01:46 +0530

    Bug Fix: class to className

--- components/InputForm.js
+++ components/InputForm.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function InputForm({addTodo, todoBody, setTodoBody}) {
   return (
-<section class="mt-12 max-w-5xl p-6 mx-auto bg-white shadow-xl rounded-xl">
-  <form class="flex mb-0 space-x-4" onSubmit={addTodo}>
-    <div class="flex-1">
-      <label class="sr-only" for="search"> Task </label>
+<section className="mt-12 max-w-5xl p-6 mx-auto bg-white shadow-xl rounded-xl">
+  <form className="flex mb-0 space-x-4" onSubmit={addTodo}>
+    <div className="flex-1">
+      <label className="sr-only" for="search"> Task </label>
 
       <input
-        class="w-full h-12 text-sm border-gray-100 rounded-lg"
+        className="w-full h-12 text-sm border-gray-100 rounded-lg"
         id="search"
         placeholder="Enter the Task"
         type="text"
@@ -18,9 +18,9 @@ function InputForm({addTodo, todoBody, setTodoBody}) {
       />
     </div>
 
-    <div class="flex-1 hidden sm:block">
-      <label class="sr-only" htmlFor="team"> Team </label>
-      <select class="w-full h-12 text-sm border-gray-100 rounded-lg" id="team" onChange={(e) => setTodoBody({...todoBody, team: e.target.value})}>
+    <div className="flex-1 hidden sm:block">
+      <label className="sr-only" htmlFor="team"> Team </label>
+      <select className="w-full h-12 text-sm border-gray-100 rounded-lg" id="team" onChange={(e) => setTodoBody({...todoBody, team: e.target.value})}>
         <option value="" selected disabled>Select Team</option>
         <option value="HR">HR</option>
         <option value="Engineering">Engineering</option>
@@ -31,7 +31,7 @@ function InputForm({addTodo, todoBody, setTodoBody}) {
 
     <div>
       <button
-        class="inline-flex items-center justify-center w-auto bg-blue h-12 px-12 text-white bg-blue-700 rounded-lg"
+        className="inline-flex items-center justify-center w-auto bg-blue h-12 px-12 text-white bg-blue-700 rounded-lg"
         type="submit"
       >
         Add Task
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
 export default function Home() {
 return (
-  <section class="bg-gray-50">
-    <div class="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
-      <div class="max-w-xl mx-auto text-center">
-        <h1 class="text-3xl font-extrabold sm:text-5xl mb-4">
+  <section className="bg-gray-50">
+    <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
+      <div className="max-w-xl mx-auto text-center">
+        <h1 className="text-3xl font-extrabold sm:text-5xl mb-4">
           Todo List
-          <strong class="font-extrabold text-blue-700 lg:block">
+          <strong className="font-extrabold text-blue-700 lg:block">
             Increase Productivity With Todo List
           </strong>
         </h1>
-        <p class="mt-4 sm:leading-relaxed sm:text-xl">
+        <p className="mt-4 sm:leading-relaxed sm:text-xl">
           Mini todo list application to help you manage your tasks.
         </p>
-        <div class="flex flex-wrap justify-center gap-4 mt-8">
-          <a href="/todo" class="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 rounded shadow sm:w-auto active:bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring">
+        <div className="flex flex-wrap justify-center gap-4 mt-8">
+          <a href="/todo" className="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 rounded shadow sm:w-auto active:bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring">
             Get Started
           </a>
-          <a class="block w-full px-12 py-3 text-sm font-medium text-blue-600 rounded shadow sm:w-auto hover:text-blue-700 active:text-blue-500 focus:outline-none focus:ring" href="/about">
+          <a className="block w-full px-12 py-3 text-sm font-medium text-blue-600 rounded shadow sm:w-auto hover:text-blue-700 active:text-blue-500 focus:outline-none focus:ring" href="/about">
             Learn More
           </a>
         </div>
